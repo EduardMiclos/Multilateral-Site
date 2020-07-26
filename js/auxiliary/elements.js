4c0164a4aca3bd0d2165647e3d8e7e15a7081b13
EduardMiclos <59684906+EduardMiclos@users.noreply.github.com>
2020-07-26 11:00:49 +0300

    Create elements.js

---
+++ js/auxiliary/elements.js
@@ -0,0 +1,37 @@
+export let el = {
+    subjectsId: {
+        romana: "list-1",
+        matematica: "list-2",
+        biologie: "list-3",
+        engleza: "list-4",
+        franceza: "list-5",
+        fizica: "list-6",
+        programare: "list-7",
+        istorie: "list-8",
+        economie: "list-9",
+        geografie: "list-10",
+        chimie: "list-11",
+        religie: "list-12",
+        filosofie: "list-13",
+        psihologie: "list-14"
+        },
+    types: {
+        video: "&#128253;",
+        read: "&#128195;",
+        exercise: "&#9997;",
+        game: "&#127918;",
+        seemore: "&#43;",
+        see: "&#128064;",
+        playlist: "&#127902;&#65039;"
+    },
+    game: {
+        starter: 'starter',
+        container: ".game-container",
+        options: '.options',
+        option: 'option',
+        score: 'score',
+        SC_GENIUS: `game_img/genius.png`,
+        SC_MED: `game_img/med.png`,
+        SC_INF: `game_img/inf.png`
+    }
+};
