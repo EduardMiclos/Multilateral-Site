be701a024fc4c32fb1998d3cb8e5df461baa2330
EduardMiclos <59684906+EduardMiclos@users.noreply.github.com>
2020-09-27 11:02:23 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,13 +1,17 @@
 import  * as createSubjects from './auxiliary/createdSubjects'
 import  {gamePlay} from './auxiliary/game'
+import {solarSystemView} from './auxiliary/solarSystem'
 
     /* Check page index and decide whether to create subject or questions for the game. */
     let PAGE = document.getElementsByTagName("html")[0].id;
 
     if(PAGE === "page-1"){
         createSubjects.createSubjects();
-    }else{
+    }else if(PAGE === "page-2"){
         document.querySelector(".joaca").addEventListener(`click`, ()=>{
             setTimeout(gamePlay, 200);
         })
+    }else {
+        solarSystemView();
     };
+
