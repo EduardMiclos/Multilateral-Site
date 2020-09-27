9288d6f8eaf1ea6b6139028f2f3ed65d639222cf
EduardMiclos <59684906+EduardMiclos@users.noreply.github.com>
2020-09-27 11:05:36 +0300

    Update game.php

--- game.php
+++ game.php
@@ -2,24 +2,29 @@
 <html lang="ro" id="page-2">
     <head>
         <title>Multilateral</title>
-        <link
-        href="https://fonts.googleapis.com/css?family=Oswald:300&display=swap"
-        rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css?family=Oswald:300&display=swap" rel="stylesheet"/>
         <link href="https://fonts.googleapis.com/css?family=Bebas+Neue&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300&display=swap" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="css/gamestyle.css">
 	<meta charset="UTF-8" />
     </head>
 
     <body>
         <header>
-            <a href="index.html"><img class="title" src="img/logovara.png" alt="MULTILATERAL"></a>
+            <img class="title" src="img/logo.png" alt="MULTILATERAL">
+            <div class="header-container">
+                <a href="index.html" class="game-button"><button class="game-button">Înapoi pe pagina principală</button></a>
+                <a href="solarSystem.html" class="game-button"><button class="game-button">Învață Sistemul Solar</button></a>
+            </div>
         </header>
+
         <div class="game-container">
             <p class="title-about">AFLĂ DACĂ EȘTI UN <span style="color: white;">UOMO UNIVERSALE</span></p>
             <img class="davinci" src="game_img/introdavinci.png">
             <h1>BINE AI VENIT! AI FĂCUT O ALEGERE ÎNȚELEAPTĂ! SĂ VEDEM CE ȘTII. APASĂ PE <span style="color: teal;">JOACĂ</span> PENTRU A ÎNCEPE.</h1>
             <button class="joaca">JOACĂ</button>
         </div>
+
         <div id="score">PUNCTAJ: <span style="color: teal;"></span></div>
         <script type="text/javascript" src="js/bundle07292020.js"></script></body>
-        </html>
+    </html>
