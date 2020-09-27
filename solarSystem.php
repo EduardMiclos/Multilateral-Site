776bd08e87483150fbf5136881a43eb440d39997
EduardMiclos <59684906+EduardMiclos@users.noreply.github.com>
2020-09-27 11:01:55 +0300

    Create solarSystem.php

---
+++ solarSystem.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en" id="page-3">
+<head>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:wght@300&display=swap" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/solarSystem.css">
+    <title>Sistemul Solar</title>
+</head>
+<body>
+
+    <header>
+        <img class="title" src="img/logo.png" alt="MULTILATERAL">
+
+        <div class="header-container">
+            <a href="game.html" class="game-button"><button class="game-button">Testează-ți cultura generală</button></a>
+            <a href="index.html" class="game-button"><button class="game-button">Înapoi pe pagina principală</button></a>
+        </div>
+    </header>
+
+
+<div class="container">
+    <span class="tips">Fii curios. Apasă pe fiecare planetă.</span>
+    <div class="planets">
+
+        <div class="star"></div>
+        <div class="text-message fadeOut invisible"> <img src="https://cdn.mos.cms.futurecdn.net/m2FG3yRCxLrjmMV8xABx4C.jpg" class="info-image"></img>
+            <strong style="color:#d1d14d;">Steaua Sirius</strong> este cea mai strălucitoare stea observată cu ochiul liber pe cer.</div>
+        <div class="sphere sun"></div>
+        <div class="text-message fadeOut invisible"><img src="https://assets.newatlas.com/dims4/default/25a74b1/2147483647/strip/true/crop/2000x1333+0+83/resize/1200x800!/quality/90/?url=http%3A%2F%2Fnewatlas-brightspot.s3.amazonaws.com%2F25%2Fc8%2Fb5ab97574f8c96ea6e298ea68d0e%2Fdepositphotos-13469325-l-2015.jpg" class="info-image"></img>
+        <strong style="color:#d1d14d;">Soarele</strong> este steaua din centrul 
+        <span style="color: #d1d14d">Sistemului Solar</span>. Este o sferă aproape perfectă din plasmă fierbinte și cea mai importantă sursă de 
+        energie pentru viața de pe Pământ. Toate planetele din Sistemul Solar orbitează 
+        (se rotesc) în jurul Soarelui.Soarele se află aproape de marginea interioară a
+        Brațului Orion a Căii Lactee.</div>
+
+
+        <div class="sphere mercury"></div>
+        <div class="text-message fadeOut invisible"> <img src="https://cdn.mos.cms.futurecdn.net/MTEiJvP99DScN3vkAsE9LA-320-80.jpg" class="info-image"></img>
+            <strong style="color:#d1d14d;">Mercur</strong> este planeta cea mai apropiată de Soare, 
+            înconjurându-l o dată la fiecare 88 de zile pământene. Planeta Mercur este similară în 
+            aparență cu Luna, fiind împânzită de cratere. Ea nu are sateliți naturali și nici o 
+            atmosferă mai densă. Observații înregistrate ale planetei Mercur datează din vremea 
+            sumerienilor, în al treilea mileniu înaintea erei noastre. Romanii au numit planeta 
+            după zeul mesager Mercur (în Grecia Hermes, în Babilonia Nabu), probabil datorită 
+            mișcării aparent rapide pe cerul crepuscular.</div>
+
+
+        <div class="sphere venus"></div>
+        <div class="text-message fadeOut invisible"> <img src="https://cdn.mos.cms.futurecdn.net/otFCeKHpqqqgKdg9WhzUR4.jpg" 
+            class="info-image"></img><strong style="color:#d1d14d;">Venus</strong>
+            este a doua planetă ca distanță față de Soare în sistemul nostru solar. 
+            Situată la circa 108 milioane km de Soare, Venus își parcurge orbita în 224,7 de zile. 
+            Rotația în jurul propriei sale axe este foarte lentă, durează 243 de zile și are loc de
+            la est la vest, în sens invers față de rotația celorlalte planete. Venus este una din
+            cele patru planete terestre (telurice) solare, în sensul că, la fel ca Pământul, 
+            este un corp ceresc cu suprafață solidă. Mărimea și masa sunt foarte asemănătoare cu
+            Pământul și este adesea descris ca „sora” Pământului, sau „fratele geamăn”.</div>
+
+        <div class="sphere terra"></div>
+        <div class="text-message fadeOut invisible"> <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/The_Earth_seen_from_Apollo_17.jpg/450px-The_Earth_seen_from_Apollo_17.jpg"
+            class="info-image"></img><strong style="color:#d1d14d;">Pământul (Terra, Planeta Albastră)</strong> 
+            este casa noastră. Este a treia planetă de la Soare și cea mai mare dintre planetele telurice ale Sistemului Solar, atât 
+            pentru masă, cât și pentru diametru. S-a format în urmă cu aproximativ 4,5 miliarde de ani. Este 
+            singurul corp ceresc despre care se știe că adăpostește viață. Forma Pământului este, aproximativ, 
+            sferică. Datorită rotației sale, Pământul este turtit la poli și bombat la ecuator.</div>
+
+        <div class="sphere moon"></div>
+        <div class="text-message fadeOut invisible"> <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/FullMoon2010.jpg/1024px-FullMoon2010.jpg" 
+            class="info-image"></img><strong style="color:#d1d14d;">Luna</strong> 
+            este <span style="color: #bbedec">satelitul natural</span> al Pământului. 
+            Un satelit natural este un corp ceresc care se rotește în jurul unei planete. Se consideră că
+            Luna s-a format acum circa 4,51 miliarde de ani, nu mult după Pământ. Există mai multe ipoteze
+            pentru originea sa; cea mai acceptată explicație este că Luna s-a format din resturile rămase
+            după un impact uriaș între Pământ și un corp de dimensiunile lui Marte, numit Theia. Luna este 
+            în rotație sincronă cu Pământul, adică arată întotdeauna aceeași față către e</div>
+
+        <div class="sphere mars"></div>
+        <div class="text-message fadeOut invisible"> <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/OSIRIS_Mars_true_color.jpg/600px-OSIRIS_Mars_true_color.jpg" 
+            class="info-image"></img><strong style="color:#d1d14d;">Marte</strong> este a patra planetă de la Soare și penultima ca mărime din Sistemul Solar. Marte poartă numele zeului roman al războiului și este adesea 
+            denumită <span style="color: #e6aea8;">Planeta Roșie</span>. Marte are aproximativ jumătate din diametrul Pământului, cu o suprafață doar puțin mai mică decât suprafața totală a uscatului Pământului.
+            Marte găzduiește Muntele Olimp, cel mai mare vulcan și cel mai cunoscut munte de pe orice 
+            planetă din Sistemul Solar</div>
+
+        <div class="sphere jupiter"></div>
+        <div class="text-message fadeOut invisible"> <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Jupiter_and_its_shrunken_Great_Red_Spot.jpg/440px-Jupiter_and_its_shrunken_Great_Red_Spot.jpg" 
+            class="info-image"></img><strong style="color:#d1d14d;">Jupiter</strong> 
+            este a cincea planetă de la Soare și cea mai mare din Sistemul Solar. Este un gigant gazos cu o masă de o miime din cea a Soarelui, dar de două ori și jumătate decât masa totală a tuturor celorlalte planete din Sistemul Solar. 
+            Jupiter este unul dintre cele mai strălucitoare obiecte vizibile cu ochiul liber pe cerul nopții și a 
+            fost cunoscut civilizațiilor antice încă de pe vremea istoriei înregistrate. Este numit după zeul 
+            roman Jupiter.</div>
+
+
+        <div class="ring-front"></div>
+        <div class="sphere saturn"></div>
+        <div class="text-message fadeOut invisible"> <img src="https://upload.wikimedia.org/wikipedia/commons/c/c7/Saturn_during_Equinox.jpg" 
+            class="info-image"></img><strong style="color:#d1d14d;">Saturn</strong> este a șasea planetă de la Soare și a doua ca mărime din Sistemul Solar, după Jupiter. Împreună cu Jupiter, Uranus și Neptun, Saturn este clasificat ca 
+            un gigant compus din gaze. Saturn este numit după zeul roman Saturnus 
+            (de la care provine denumirea zilei de sâmbătă) și prezintă un sistem de inele, care sunt alcătuite din particule de gheață și 
+            mici cantități de deșeuri de praf și rocă.</div>
+            <div class="ring-behind"></div>
+
+        <div class="sphere uranus"></div>
+        <div class="text-message fadeOut invisible"> <img src="https://cdn.fanatik.ro/wp-content/thumbnails/W8HttC7UEm_YN1b2k-LyfqL3d5Q=/664x374/smart/filters:contrast(5):quality(50):format(jpeg)/wp-content/uploads/2020/08/uranus-in-taur.jpg" 
+            class="info-image"></img><strong style="color:#d1d14d;">Uranus</strong> este a șaptea planetă de la
+            Soare și a treia ca mărime (după diametru). Uranus este mai mare ca diametru însă mai mică sub 
+            aspectul masei decât Neptun. Uranus este zeitatea greacă ancestrală a Raiurilor, un zeu 
+            suprem timpuriu. Plasat pe o orbită de 19 ori mai îndepărtată de 
+            Soare decât cea a Pământului, Uranus, ca și Neptun, primește foarte puțină căldură, fiind un gigant de
+            gheață.</div>
+
+        <div class="sphere neptune"></div>
+        <div class="text-message fadeOut invisible"> <img src="https://media.wired.com/photos/5d04045bde1abfe4e801d054/master/pass/Science-Neptune-FA-PIA01492_orig.jpg"
+            class="info-image"></img><strong style="color:#d1d14d;">Neptun</strong> este a opta planeta de la Soare din sistemul solar. Numită după zeul roman al mării, este a patra planetă după diametru și a treia după masă. 
+            Neptun are o masă de 17 ori mai mare decât cea a Pământului și puțin mai mare 
+            decât masa lui Uranus. Neptun a fost prima planetă găsită prin calcule matematice. Deoarece 
+            Neptun este departe de Soare, are una dintre cele mai reci atmosfere din sistemul solar, dar și 
+            unele dintre cele mai puternice vânturi din Sistemul Solar, ale căror viteze ating, conform măsurătorilor, 2100 km/h</div>
+    </div>     
+</div>
+
+<script type="text/javascript" src="js/bundle07292020.js"></script></body>
+</html>
