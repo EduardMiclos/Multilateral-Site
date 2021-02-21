30409fd6d303b453a8c339d5d27a33b8e8fefbe3
Eduard Micloș <59684906+EduardMiclos@users.noreply.github.com>
2021-02-21 22:36:22 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,17 +1,24 @@
-import  * as createSubjects from './auxiliary/createdSubjects'
+import  * as sub from './auxiliary/createSubjects'
 import  {gamePlay} from './auxiliary/game'
 import {solarSystemView} from './auxiliary/solarSystem'
+import {ProtectTheEarth} from './auxiliary/Protect the Earth/main'
 
     /* Check page index and decide whether to create subject or questions for the game. */
     let PAGE = document.getElementsByTagName("html")[0].id;
 
+
     if(PAGE === "page-1"){
-        createSubjects.createSubjects();
-    }else if(PAGE === "page-2"){
+        sub.createSubjects();
+    }
+    else if(PAGE === "page-2"){
         document.querySelector(".joaca").addEventListener(`click`, ()=>{
             setTimeout(gamePlay, 200);
         })
-    }else {
+    }
+    else if(PAGE === "page-3"){
         solarSystemView();
-    };
+    }
+    else{
+        ProtectTheEarth();
+    }
 
