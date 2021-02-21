6e83f07081b5c983d8eef7056b5b6be938215859
Eduard Micloș <59684906+EduardMiclos@users.noreply.github.com>
2021-02-21 22:44:43 +0200

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
         <link href="https://fonts.googleapis.com/css?family=Dancing+Script|Girassol|Open+Sans+Condensed:300|Roboto+Condensed&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Dosis&display=swap" rel="stylesheet">
         <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha256-3dkvEK0WLHRJ7/Csr0BZjAWxERc5WH7bdeUya2aXxdU= sha512-+L4yy6FRcDGbXJ9mPG8MT/3UCDzwR9gPeyFNMCtInsol++5m3bk2bXWKdZjvybmohrAsn3Ua5x8gfLnbE1YkOg==" crossorigin="anonymous">
-        <link rel="stylesheet" type="text/css" href="css/winter-update-style.css">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
         <meta charset="UTF-8" />
     </head>
 
